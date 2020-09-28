fae7264760e4949f87b15237ab26bc7e5809ac90
thebigsasha <sashathethird@gmail.com>
2020-09-28 12:44:33 -0400

    Initial commit

---
+++ src/TheScicilian/ListOfLinks.java
@@ -0,0 +1,68 @@
+package TheScicilian;
+
+public class ListOfLinks<E> {
+    public ListOfLinks() {
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+
+
+    public ListNode<E> getFirst(){
+
+    }
+
+    public int size(){                                          //Returns the number of elements in the list by iterating thru and counting
+
+    }
+
+    public E get(int index) throws IndexOutOfBoundsException{   //Gets a node at an index
+
+
+    }
+    public void add(E data){                                    //sets head if head is null, else adds a node
+    }
+
+
+
+    public ListNode<E> getLast(){                               //iterates thru the list till the node's next is null (last node)
+
+    }
+
+    public void removeLast(){
+    }
+
+    public String toString(){
+                                                                //Print all the stuff in this list, comma separated!
+    }
+}
+
+
+    private class ListNode<E>{                                  //List node: stores a bit of data and a pointer to next
+
+
+        public ListNode(E data){
+
+        }
+
+        public ListNode<E> next(){
+
+        }
+
+        public void setNext(ListNode<E> data) {
+
+        }
+
+        public E getData() {
+
+        }
+
+        public void setData(E data){
+
+        }
+
+}
+
+
