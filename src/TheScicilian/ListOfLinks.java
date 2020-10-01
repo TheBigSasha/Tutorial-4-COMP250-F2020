c5d59d88e610e42f6a4027c95dddd467f1d77a83
thebigsasha <sashathethird@gmail.com>
2020-10-01 16:45:48 -0400

    Post solutions in realtime????

--- src/TheScicilian/ListOfLinks.java
+++ src/TheScicilian/ListOfLinks.java
@@ -1,68 +1,117 @@
 package TheScicilian;
 
+import java.util.List;
+
 public class ListOfLinks<E> {
+    private ListNode<E> head;
+    private int size;
+
     public ListOfLinks() {
-
+        head = null;
+        size = 0;
     }
 
-    public boolean isEmpty(){
-
-    }
-
-
-    public ListNode<E> getFirst(){
-
-    }
-
-    public int size(){                                          //Returns the number of elements in the list by iterating thru and counting
-
-    }
-
-    public E get(int index) throws IndexOutOfBoundsException{   //Gets a node at an index
-
-
-    }
-    public void add(E data){                                    //sets head if head is null, else adds a node
+    public boolean isEmpty() {
+        return size == 0 || head == null;
     }
 
 
+    public ListNode<E> getFirst() {
+        return head;
+    }
 
-    public ListNode<E> getLast(){                               //iterates thru the list till the node's next is null (last node)
+    public int size() {                                          //Returns the number of elements in the list by iterating thru and counting
+        return size;
+        //Also works if you have a while loop going through the list and counting how many times you need to go to count the whole list
+        //If you have a system where you can combine lists directly by manipulating nodes, you may not always be able to trust int size.
+    }
+
+    public E get(int index) throws IndexOutOfBoundsException {   //Gets a node at an index
+        if(index > this.size() - 1 || index < 0) throw new IndexOutOfBoundsException("Index " + index + " out of bounds for list of size " + size());
+
+        int counter = 0;
+        E data = null;
+        ListNode<E> temp = head;
+        while(temp != null){
+            if(counter == index){
+                return temp.getData();
+            }
+            temp = temp.next;
+            counter++;
+        }
+        return null;
+    }
+
+    private ListNode<E> getNode(int index) throws IndexOutOfBoundsException {   //Gets a node at an index
+        if(index > this.size() - 1 || index < 0) throw new IndexOutOfBoundsException("Index " + index + " out of bounds for list of size " + size());
+
+        int counter = 0;
+        E data = null;
+        ListNode<E> temp = head;
+        while(temp != null){
+            if(counter == index){
+                return temp;
+            }
+            temp = temp.next;
+            counter++;
+        }
+        return null;
+    }
+
+    public void add(E data) {                                    //sets head if head is null, else adds a node'
+        ListNode<E> newNode = new ListNode<>(data);         // We create a new node from the data we are given
+        getLastNode().setNext(newNode);                     // We go to the last node and change the reference from null to the new node
+        size++;                                             // We track our size
+    }
+
+
+    private ListNode<E> getLastNode() {                               //iterates thru the list till the node's next is null (last node)
+        ListNode<E> temp = head;
+        while(temp.next()!=null){
+            temp = temp.next();
+        }
+        return temp;
+    }
+
+    public E getLast(){
+        return get(size() - 1);
+    }
+
+    public void removeLast() {
+        this.getNode(size() - 2).setNext(null);
+        size--;
+    }
+
+    public String toString() {
+        //Print all the stuff in this list, comma separated!
 
     }
 
-    public void removeLast(){
-    }
 
-    public String toString(){
-                                                                //Print all the stuff in this list, comma separated!
-    }
-}
+    private class ListNode<E> {                                  //List node: stores a bit of data and a pointer to next
+        ListNode<E> next;
+        E data;
 
-
-    private class ListNode<E>{                                  //List node: stores a bit of data and a pointer to next
-
-
-        public ListNode(E data){
-
+        public ListNode(E data) {
+            this.data = data;
+            this.next = null;
         }
 
-        public ListNode<E> next(){
-
+        public ListNode<E> next() {
+            return next;
         }
 
-        public void setNext(ListNode<E> data) {
-
+        public void setNext(ListNode<E> next) {
+            this.next = next;
         }
 
         public E getData() {
-
+            return data;
         }
 
-        public void setData(E data){
-
+        public void setData(E data) {
+            this.data = data;
         }
 
+    }
 }
-
-
